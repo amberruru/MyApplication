3119e1283731caddaba42947787ede9f6bd8d4cf
jogol <295528584@qq.com>
2017-12-19 18:01:30 +0800

    后台定时更新，weatheractivity使用buttonknife

---
+++ app/src/main/java/com/weather/Service/AutoUpdateService.java
@@ -0,0 +1,76 @@
+package com.weather.Service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.support.annotation.IntDef;
+import android.widget.Toast;
+
+import com.weather.activity.WeatherActivity;
+import com.weather.gson.Weather;
+import com.weather.util.HttpUtil;
+import com.weather.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class AutoUpdateService extends Service {
+    public AutoUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+       return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,int flags, int startId) {
+        updateWeather();
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 60*1000;
+        long triggerAtTime = SystemClock.elapsedRealtime()+anHour;
+        Intent i = new Intent(this,AutoUpdateService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(this,0,i,0);
+        alarmManager.cancel(pendingIntent);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pendingIntent);
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    public void updateWeather(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final String weather = (String) sharedPreferences.getString("weather",null);
+        if (null != weather){
+            Weather weather1 = Utility.handleWeahterResponse(weather);
+            String weatherId = weather1.basic.weatherId;
+            String url = "hppt://guolin.tech/api/weather?cityid="+weatherId+"&key=e1059b2eaec64cab9e37bed6bec776d6";
+            HttpUtil.sendOkHttpRequest(url, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String responseText = response.body().string();
+                    Weather weather2 = Utility.handleWeahterResponse(responseText);
+                    if (null!=weather2&&"ok".equals(weather2.status)){
+                        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather",responseText);
+                        editor.apply();
+                        Toast.makeText(AutoUpdateService.this,"定时更新天气完成",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+}
--- app/src/main/java/com/weather/activity/WeatherActivity.java
+++ app/src/main/java/com/weather/activity/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.weather.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.administrator.myapplication.R;
+import com.weather.Service.AutoUpdateService;
 import com.weather.gson.Forecast;
 import com.weather.gson.Weather;
 import com.weather.util.HttpUtil;
@@ -27,52 +29,58 @@ import com.weather.util.Utility;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    @BindView(R.id.weather_layout)
     private ScrollView weahterLayout;
+    @BindView(R.id.title_city)
     private TextView titleCity;
+    @BindView(R.id.title_update_time)
     private TextView titleUpdateTime;
+    @BindView(R.id.degree_text)
     private TextView degreeText;
+    @BindView(R.id.weather_info_text)
     private TextView weatherInfoText;
+    @BindView(R.id.forecast_layout)
     private LinearLayout forecastLayout;
+    @BindView(R.id.aqi_text)
     private TextView aqiText;
+    @BindView(R.id.pm25_text)
     private TextView pm25Text;
+    @BindView(R.id.comfort_text)
     private TextView comfortText;
+    @BindView(R.id.car_wash_text)
     private TextView carWashText;
+    @BindView(R.id.sport_text)
     private TextView sportText;
+    @BindView(R.id.bing_img)
     private ImageView imageView;
+    @BindView(R.id.swipe_refresh)
     public SwipeRefreshLayout swipeRefreshLayout;//下拉刷新
+    @BindView(R.id.nav_btn)
     private Button navBtn;//更换地址
+    @BindView(R.id.drawer_layout)
     public DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weahter);
+        //激活定时更新
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
         //根据系统版本背景图片是否包含住状态栏
         if (Build.VERSION.SDK_INT>=21){
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-        weahterLayout = (ScrollView) findViewById(R.id.weather_layout);
-        titleCity = (TextView) findViewById(R.id.title_city);
-        degreeText = (TextView) findViewById(R.id.degree_text);
-        weatherInfoText = (TextView) findViewById(R.id.weather_info_text);
-        forecastLayout = (LinearLayout) findViewById(R.id.forecast_layout);
-        aqiText = (TextView) findViewById(R.id.aqi_text);
-        pm25Text = (TextView) findViewById(R.id.pm25_text);
-        comfortText = (TextView) findViewById(R.id.comfort_text);
-        carWashText = (TextView) findViewById(R.id.car_wash_text);
-        sportText = (TextView) findViewById(R.id.sport_text);
-        titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
-        imageView = (ImageView) findViewById(R.id.bing_img);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
-        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
         final String weatherId ;
         //是否有缓存
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -94,21 +102,14 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
         //背景图
-        /*String bingImg = prefs.getString("bing_img",null);
-        if (bingImg != null) {
-            Glide.with(this).load(bingImg).into(imageView);
-        }else{*/
-            loadBingImg();
-        /*}*/
+         loadBingImg();
         //换地址
-        navBtn = (Button) findViewById(R.id.nav_btn);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(Gravity.START);
-            }
-        });
+
+    }
+
+    @OnClick(R.id.nav_btn)
+    public void change_area(){
+        drawerLayout.openDrawer(Gravity.START);
     }
 
     public void requestWeahter(String weatherId){
